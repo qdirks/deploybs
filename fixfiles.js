f5ea2ed3e1a64a1daa58c3cbc02121a6595eceed
Quinn Dirks <qdirks@gmail.com>
2023-02-18 03:05:57 -0600

    save

--- fixfiles.js
+++ fixfiles.js
@@ -1,47 +1,51 @@
 const fs = require("fs");
-const transforms = Object.create(null);
-function transform(fileName) {
-    if (!transforms[fileName]) transforms[fileName] = [];
-    return transforms[fileName];
+const transformsMap = Object.create(null);
+/**
+ * @param {string} fileName 
+ * @returns {function[]}
+ */
+function getTransforms(fileName) {
+    if (!transformsMap[fileName]) transformsMap[fileName] = [];
+    return transformsMap[fileName];
 }
-transform("packages/browser-sync/package.json").push(
+getTransforms("packages/browser-sync/package.json").push(
     redirectDependencies
 );
-transform("packages/browser-sync-client/dist/index.js").push(
+getTransforms("packages/browser-sync-client/dist/index.js").push(
     fixLineEndings
 );
-transform("packages/browser-sync-ui/public/js/app.js").push(
+getTransforms("packages/browser-sync-ui/public/js/app.js").push(
     fixLineEndings
 );
 ["browser-sync-client", "browser-sync-ui", "browser-sync"]
 .forEach(packageName=>{
-    transform("packages/" + packageName + "/package-lock.json").push(
+    getTransforms("packages/" + packageName + "/package-lock.json").push(
         fixLineEndings
     );
-    transform("packages/" + packageName + "/package.json").push(
+    getTransforms("packages/" + packageName + "/package.json").push(
         removePackageScripts,
         fixLineEndings
     );
 });
-Object.keys(transforms).forEach(fileName=>{
-    fs.readFile(fileName, (err, buf)=>{
-        if (err) return;
-        const writeStr = transforms[fileName].reduce((pv, cv)=>{
-            return cv(pv);
-        }, buf.toString());
-        fs.writeFile(fileName, writeStr, ()=>{});
-    });
+// apply the transforms to the files
+Object.keys(transformsMap).forEach(fileName=>{
+    fs.readFile(fileName, (err, buf)=>!err &&
+    fs.writeFile(fileName, transformsMap[fileName].reduce(
+        (pv, cv)=>cv(pv), buf.toString()), ()=>{} ));
 });
+/** @param {string} str */
 function fixLineEndings(str) {
     if (str.indexOf('\r') === -1) return str.split('\n').join('\r\n');
     else return str;
 }
+/** @param {string} str */
 function redirectDependencies(str) {
     const obj = JSON.parse(str);
     obj.dependencies["browser-sync-client"] = "https://github.com/qdirks/browser-sync.git#browser-sync-client";
     obj.dependencies["browser-sync-ui"] = "https://github.com/qdirks/browser-sync.git#browser-sync-ui";
     return JSON.stringify(obj, void 0, '\t');
 }
+/** @param {string} str */
 function removePackageScripts(str) {
     const obj = JSON.parse(str);
     delete obj.scripts;
